421f71b8714cb5f7b97c69ab0848f66aa62f520f
MineRock <Collapse4276@proton.me>
2022-12-28 13:09:22 +0530

    Initial Git Repo

---
+++ user-overrides.js
@@ -0,0 +1,4 @@
+user_pref("browser.startup.page", 1);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("browser.newtabpage.enabled", true);
+user_pref("browser.search.region", "US");
