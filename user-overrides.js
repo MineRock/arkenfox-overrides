5a3fd2a2e67e8623a15aa1c1107ab419e6aca147
MineRock <Collapse4276@proton.me>
2023-01-03 16:54:24 +0530

    Removed search suggestions from overrides

--- user-overrides.js
+++ user-overrides.js
@@ -1,8 +1,6 @@
 user_pref("browser.startup.page", 3); // 0102
 user_pref("privacy.clearOnShutdown.history", false); // 2811
-user_pref("keyword.enabled", false);
-user_pref("browser.search.suggest.enabled", false);
-user_pref("browser.urlbar.suggest.searches", false);
+user_pref("keyword.enabled", true);
 user_pref("network.http.referer.XOriginPolicy", 2);
 user_pref("signon.rememberSignons", false);
 
