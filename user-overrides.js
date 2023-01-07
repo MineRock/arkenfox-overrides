c8da9d3f7c8956cee34c89ee52a3dcafdcb8c2bb
MineRock <Collapse4276@proton.me>
2023-01-07 10:11:39 +0530

    Removed history saving from overrides

--- user-overrides.js
+++ user-overrides.js
@@ -1,7 +1,6 @@
 user_pref("browser.startup.page", 1);
 user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.newtabpage.enabled", true);
-user_pref("privacy.clearOnShutdown.history", false);
 user_pref("keyword.enabled", true);
 user_pref("network.http.referer.XOriginPolicy", 0);
 user_pref("signon.rememberSignons", false);
