3a59f1a781d3ac8bcfbbaf82566644668a340535
MineRock <Collapse4276@proton.me>
2023-01-17 13:52:52 +0530

    Added history saving to overrides

--- user-overrides.js
+++ user-overrides.js
@@ -4,4 +4,4 @@ user_pref("browser.newtabpage.enabled", true);
 user_pref("keyword.enabled", true);
 user_pref("network.http.referer.XOriginPolicy", 0);
 user_pref("signon.rememberSignons", false);
-
+user_pref("privacy.clearOnShutdown.history", false);
