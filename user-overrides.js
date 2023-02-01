a9975b77394fb8f364e255a375836df99cad006a
MineRock <Collapse4276@proton.me>
2023-02-01 10:07:31 +0530

    feat: set default pdf.js size page-width

--- user-overrides.js
+++ user-overrides.js
@@ -4,3 +4,4 @@ user_pref("browser.safebrowsing.phishing.enabled", false); //0401
 user_pref("layout.css.font-visibility.private", 1); //1402
 user_pref("layout.css.font-visibility.standard", 1); //1402
 user_pref("layout.css.font-visibility.trackingprotection", 1); //1402
+user_pref("pdfjs.defaultZoomValue", "page-width"); //custom: set default pdf page width to fit width
